f555d4666e7c249b1750491c4ed3c2ae0b5c7e59
Google Developer Student Club @ NJIT <58719016+gdsc-njit@users.noreply.github.com>
2023-11-20 20:22:44 -0500

    Create main.java

---
+++ 412 Fizz Buzz/main.java
@@ -0,0 +1,22 @@
+// Time: O(N)
+// Space: O(N)
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<String>();
+
+        for (int num = 1; num <= n; num++) {
+            boolean divisibleBy3 = (num % 3 == 0);
+            boolean divisibleBy5 = (num % 5 == 0);
+
+            String numAnsStr = "";
+            if (divisibleBy3) numAnsStr += "Fizz";
+            if (divisibleBy5) numAnsStr += "Buzz";
+            if (numAnsStr.equals("")) numAnsStr += Integer.toString(num);
+
+            ans.add(numAnsStr);
+        }
+
+        return ans;
+    }
+}
